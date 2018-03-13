b847dfe58d67bc163fcb797111dda3062d421562
Hagan <haganpratt@gmail.com>
2018-03-13 13:18:02 -0400

    bike

---
+++ client/src/app/listings/bike.ts
@@ -0,0 +1,7 @@
+export class Bike {
+    title: String = '';
+    description: String = '';
+    price: String = '';
+    location: String = '';
+    url: String = '';
+}
---
+++ client/src/app/users/user.ts
@@ -0,0 +1,7 @@
+export class User {
+    firstname: String = '';
+    lastname: String = '';
+    email: String = '';
+    password: String = '';
+
+}
